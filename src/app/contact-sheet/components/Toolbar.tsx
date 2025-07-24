06304fe31483d4fb6811d4cc8a0483ae31cab9b4
Pedro Duarte <oi@ped.ro>
2025-07-25 00:09:54 +0200

    update icons

--- src/app/contact-sheet/components/Toolbar.tsx
+++ src/app/contact-sheet/components/Toolbar.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import * as ToggleGroup from '@radix-ui/react-toggle-group';
 import * as Tooltip from '@radix-ui/react-tooltip';
+import { MagnifyingGlassIcon, TrashIcon } from '@radix-ui/react-icons';
 import {
-  StopIcon,
-  CircleIcon,
-  EyeNoneIcon,
-  Cross1Icon,
-  MagnifyingGlassIcon,
-  TrashIcon,
-  IdCardIcon,
-  ShadowOuterIcon,
-} from '@radix-ui/react-icons';
+  ActionRectangleIcon,
+  ActionCrossIcon,
+  ActionScribbleIcon,
+  ActionCircleIcon,
+  ActionTwinCheckIcon,
+  ActionDotIcon,
+} from './Icons';
 
 interface ToolbarProps {
   selectedAction: string;
@@ -47,89 +46,91 @@ export const Toolbar = ({
   hideLoupeOption = false,
 }: ToolbarProps) => {
   return (
-    <div className="flex items-center space-x-2 text-sm">
-      <ToggleGroup.Root
-        type="single"
-        value={selectedAction}
-        onValueChange={value => {
-          onActionChange(value || '');
-        }}
-        className="flex gap-1"
-      >
-        <TooltipWrapper content="Rectangle (R)">
+    <ToggleGroup.Root
+      className="flex gap-1 items-center space-x-2 text-sm"
+      type="single"
+      value={selectedAction}
+      onValueChange={value => {
+        onActionChange(value || '');
+      }}
+    >
+      <TooltipWrapper content="Rectangle (R)">
+        <ToggleGroup.Item
+          value="rectangle"
+          className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
+        >
+          <ActionRectangleIcon className="h-4" />
+        </ToggleGroup.Item>
+      </TooltipWrapper>
+
+      <TooltipWrapper content="Circle (C)">
+        <ToggleGroup.Item
+          value="circle"
+          className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
+        >
+          <ActionCircleIcon className="h-4" />
+        </ToggleGroup.Item>
+      </TooltipWrapper>
+
+      <TooltipWrapper content="Scribble (S)">
+        <ToggleGroup.Item
+          value="scribble"
+          className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
+        >
+          <ActionScribbleIcon className="h-4" />
+        </ToggleGroup.Item>
+      </TooltipWrapper>
+
+      <TooltipWrapper content="Cross (X)">
+        <ToggleGroup.Item
+          value="cross"
+          className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
+        >
+          <ActionCrossIcon className="h-4" />
+        </ToggleGroup.Item>
+      </TooltipWrapper>
+
+      <span className="h-[16px] w-[1px] bg-white/50" />
+
+      <TooltipWrapper content="Twin Check Sticker">
+        <ToggleGroup.Item
+          value="sticker-twin-check"
+          className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
+        >
+          <ActionTwinCheckIcon className="h-4" />
+        </ToggleGroup.Item>
+      </TooltipWrapper>
+
+      <TooltipWrapper content="Dot Sticker">
+        <ToggleGroup.Item
+          value="sticker-dot"
+          className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
+        >
+          <ActionDotIcon className="h-4" />
+        </ToggleGroup.Item>
+      </TooltipWrapper>
+
+      <span className="h-[16px] w-[1px] bg-white/50" />
+
+      <TooltipWrapper content="Delete Frame(D)">
+        <ToggleGroup.Item
+          value="delete"
+          className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-red-500 aria-checked:text-white aria-checked:hover:bg-red-500 rounded flex items-center"
+        >
+          <TrashIcon className="h-4" />
+        </ToggleGroup.Item>
+      </TooltipWrapper>
+
+      {!hideLoupeOption && (
+        <TooltipWrapper content="Loupe (L)">
           <ToggleGroup.Item
-            value="rectangle"
+            value="loupe"
             className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
           >
-            <StopIcon className="w-4 h-4" />
+            <MagnifyingGlassIcon className="h-4" />
           </ToggleGroup.Item>
         </TooltipWrapper>
-
-        <TooltipWrapper content="Circle (C)">
-          <ToggleGroup.Item
-            value="circle"
-            className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
-          >
-            <CircleIcon className="w-4 h-4" />
-          </ToggleGroup.Item>
-        </TooltipWrapper>
-
-        <TooltipWrapper content="Scribble (S)">
-          <ToggleGroup.Item
-            value="scribble"
-            className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
-          >
-            <EyeNoneIcon className="w-4 h-4" />
-          </ToggleGroup.Item>
-        </TooltipWrapper>
-
-        <TooltipWrapper content="Cross (X)">
-          <ToggleGroup.Item
-            value="cross"
-            className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
-          >
-            <Cross1Icon className="w-4 h-4" />
-          </ToggleGroup.Item>
-        </TooltipWrapper>
-
-        <TooltipWrapper content="Twin Check Sticker">
-          <ToggleGroup.Item
-            value="sticker-twin-check"
-            className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
-          >
-            <IdCardIcon className="w-4 h-4" />
-          </ToggleGroup.Item>
-        </TooltipWrapper>
-
-        <TooltipWrapper content="Dot Sticker">
-          <ToggleGroup.Item
-            value="sticker-dot"
-            className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
-          >
-            <ShadowOuterIcon className="w-4 h-4" />
-          </ToggleGroup.Item>
-        </TooltipWrapper>
-
-        <TooltipWrapper content="Delete (D)">
-          <ToggleGroup.Item
-            value="delete"
-            className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-red-500 aria-checked:text-white aria-checked:hover:bg-red-500 rounded flex items-center"
-          >
-            <TrashIcon className="w-4 h-4" />
-          </ToggleGroup.Item>
-        </TooltipWrapper>
-
-        {!hideLoupeOption && (
-          <TooltipWrapper content="Loupe (L)">
-            <ToggleGroup.Item
-              value="loupe"
-              className="px-2 py-1 text-xs text-white hover:bg-white/20 focus:outline-none aria-checked:bg-white aria-checked:text-black aria-checked:hover:bg-white rounded flex items-center"
-            >
-              <MagnifyingGlassIcon className="w-4 h-4" />
-            </ToggleGroup.Item>
-          </TooltipWrapper>
-        )}
-      </ToggleGroup.Root>
-    </div>
+      )}
+    </ToggleGroup.Root>
   );
 };
